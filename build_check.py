5e28f1ec99d1cd3a737e002513cf2f08529cbcbe
ifracheran <ifrach.eran@gmail.com>
2019-06-20 20:06:11 +0300

    Update build_check.py

--- build_check.py
+++ build_check.py
@@ -2,7 +2,7 @@ import os
 
 build = int(os.environ.get('BUILD_NUMBER'))
 
-
+print (f'job name is: {os.environ.get("JOB_NAME")}')
 if not build%3 == 0:
     print (f'Error: build ran {build} times')
     exit(1)
